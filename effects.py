dba58845f50028026b0df94dc901f4663f48be9c
alpoc <dwiebold7@gmail.com>
2022-10-18 19:19:16 +0100

    bug fixes + halloween

--- effects.py
+++ effects.py
@@ -4,16 +4,16 @@ import neopixel
 from rainbowio import colorwheel
 import random
 import time
+import numpy as np
 
 
 PIXEL_PIN = board.D21 # gpio number, D21 is pin 40
 global NUM_PIXELS
-NUM_PIXELS = 200
+NUM_PIXELS = 150
 BRIGHTNESS = 1 # 0.0 - 1
 PIXEL_ORDER = neopixel.RGB
 # PIXEL_ORDER = neopixel.BRG
 
-global pixels
 pixels = neopixel.NeoPixel(PIXEL_PIN, NUM_PIXELS, pixel_order=PIXEL_ORDER, brightness=BRIGHTNESS, auto_write=False)
 
 RED = (255, 0, 0)
@@ -37,8 +37,12 @@ def off():
 def keep_running(start_time, run_time=None):
     """
     Check if the program is intended to keep runnning.
+    -1 run time to keep running indefinetly.
+    args:
+        start_time: when the program started
+        run_time: seconds the program is intended to run for
     """
-    if run_time:
+    if run_time and run_time > 0:
         now = time.time() - start_time
         seconds = int(now % 60)
         if seconds > run_time:
@@ -47,11 +51,49 @@ def keep_running(start_time, run_time=None):
 
 
 def color_cycle(sleep_time, colors, run_count):
+    """ cycle through colors.
+        supply a single color for a pulse.
+        args:
+            colors: the colors to cycle through
+            sleep_time: how fast the effect runs
+            run_count: how many times the effect should run
+    """
     for _ in range(0, run_count):
         for color in colors:
             pixels.fill(color)
             pixels.show()
             time.sleep(sleep_time)
+
+
+def color_cycle_fade(sleep_time, colors, run_count):
+    """ cycle through colors fading in and out.
+        supply a single color for a pulse.
+        args:
+            colors: the colors to cycle through
+            sleep_time: how fast the effect runs
+            run_count: how many times the effect should run
+    """
+    for _ in range(0, run_count):
+        for color in colors:
+
+            for brightness in np.arange(0, 1, 0.1):
+                pixels.brightness = brightness / 10
+                pixels.fill(color)
+                pixels.show()
+                time.sleep(sleep_time)
+
+            for brightness in np.arange(1, 10, 0.2):
+                pixels.brightness = brightness / 10
+                pixels.fill(color)
+                pixels.show()
+                time.sleep(sleep_time)
+
+            for brightness in np.arange(10, 1, -0.2):
+                pixels.brightness = brightness / 10
+                pixels.fill(color)
+                pixels.show()
+                time.sleep(sleep_time)
+
 
 def timed_fill(sleep_time, colors, run_count=None):
     """
@@ -61,7 +103,6 @@ def timed_fill(sleep_time, colors, run_count=None):
         sleep_time: how fast the effect runs
         run_count: how many times the effect should run
     """
-
     for _ in range(0, run_count):
         for color in colors:
             for i in range(NUM_PIXELS):
@@ -98,17 +139,18 @@ def single_down(sleep_time=0, color_list=None, run_count=None):
                 time.sleep(sleep_time)
 
 
-def color_wave_full(sleep_time, colors, width=None, run_time=None):
-    """
-    itterate the colors down the chain
-    TODO: make this a linked list so that we may itterate through longer widths
-    args:
-        sleep_time: speed of the loop
-        colors: list of colors to loop through
-        width: number of same color in a row
-        run_time: how long in seconds the effect should run for
-    """
+def rainbow_cycle(sleep_time, run_time=None):
     start_time = time.time()
+    while keep_running(start_time, run_time):
+        for j in range(255):
+            for i in range(NUM_PIXELS):
+                rc_index = (i * 256 // NUM_PIXELS) + j
+                pixels[i] = colorwheel(rc_index & 255)
+            pixels.show()
+            time.sleep(sleep_time)
+
+
+def create_filled_collection(colors, width):
     color_collection = []
     color = 0
     duplicate = 1
@@ -122,6 +164,20 @@ def color_wave_full(sleep_time, colors, width=None, run_time=None):
             color += 1
             duplicate = 1
     color_col = collections.deque(color_collection)
+    return color_col
+
+def color_wave_full(sleep_time, colors, width=None, run_time=None):
+    """
+    itterate the colors down the chain
+    TODO: make this a linked list so that we may itterate through longer widths
+    args:
+        sleep_time: speed of the loop
+        colors: list of colors to loop through
+        width: number of same color in a row
+        run_time: how long in seconds the effect should run for
+    """
+    start_time = time.time()
+    color_col = create_filled_collection(colors, width)
 
     while keep_running(start_time, run_time):
         for i in range(NUM_PIXELS):
@@ -131,12 +187,61 @@ def color_wave_full(sleep_time, colors, width=None, run_time=None):
         color_col.rotate(1)
 
 
-def rainbow_cycle(sleep_time, run_time=None):
+def left_right_shift(color_list, width, shift_amount, run_time):
+    """
+    move back and forth
+    args:
+        sleep_time: speed of the loop
+        colors: list of colors to loop through
+        width: number of same color in a row
+        run_time: how long in seconds the effect should run for
+    """
     start_time = time.time()
+    color_col = create_filled_collection(color_list, width)
+    shift_direction = 1
+    shift_count = 0
+    sleep_time = 0.5
     while keep_running(start_time, run_time):
-        for j in range(255):
-            for i in range(NUM_PIXELS):
-                rc_index = (i * 256 // NUM_PIXELS) + j
-                pixels[i] = colorwheel(rc_index & 255)
-            pixels.show()
-            time.sleep(sleep_time)
+        for i in range(NUM_PIXELS):
+            pixels[i] = color_col[i]
+        pixels.show()
+        time.sleep(sleep_time)
+        color_col.rotate(shift_direction)
+        shift_count += shift_dirrection
+        if abs(shift_count) >= shift_amount:
+            shift_direction *= -1
+
+
+def water_waves(color_list, width, shift_amount, run_time):
+    """
+    two steps forward 1 step back. trying to make an beach wave effect.
+    args:
+        colors: list of colors to loop through
+        width: number of same color in a row
+        shift_amount: amount to move forward and back
+        run_time: how long in seconds the effect should run for
+    """
+    start_time = time.time()
+    color_col = create_filled_collection(color_list, width)
+    shift_direction = 1
+    shift_count = 0
+    shift_amount_neg = round(shift_amount *-1 *.6)
+    velocity_sleep = initial_sleep = 0.05
+    while keep_running(start_time, run_time):
+        for i in range(NUM_PIXELS):
+            pixels[i] = color_col[i]
+        pixels.show()
+        velocity_sleep += 0.007
+
+        time.sleep(velocity_sleep)
+        color_col.rotate(shift_direction)
+
+        shift_count += shift_direction
+        if shift_count >= shift_amount:
+            shift_direction *= -1
+            shift_count = 0
+            velocity_sleep = initial_sleep
+        elif shift_count <= shift_amount_neg:
+            shift_direction *= -1
+            shift_count = 0
+            velocity_sleep = initial_sleep
--- main.py
+++ main.py
@@ -25,11 +25,11 @@ COLOR_PACKS = [HALLOWEEN, FOURTH_OF_JULY, VALENTINES, EASTER]
 def random_effects():
 
     while True:
-        effects.off()
-        choice = random.randint(0, 4)
-        # color_pack = choice.random(COLOR_PACKS)
-        color_pack = EASTER
-        random.shuffle(color_pack)
+        # effects.off()
+        choice = random.randint(0, 6)
+        #color_pack = random.choice(COLOR_PACKS)
+        #random.shuffle(color_pack)
+        color_pack = HALLOWEEN
         if choice == 0:
             print("single_down")
             effects.single_down(0, color_pack, run_count=1)
@@ -37,13 +37,21 @@ def random_effects():
             print("full wave random width")
             width = random.randint(3, 10)
             effects.color_wave_full(0.05, color_pack, width, run_time=30)
-        elif choice == 4:
+        elif choice == 2:
             print("timed fill")
             effects.timed_fill(0.005, color_pack, run_count=3)
-        elif choice == 5:
-            print('color cyclle')
+        elif choice == 3:
+            print('color cycle')
             effects.color_cycle(1, color_pack, run_count=len(color_pack))
-
+        elif choice == 4:
+            print('left_right_shift')
+            effects.left_right_shift(color_pack, 5, 18, 60)
+        elif choice == 5:
+            print('water_waves')
+            effects.water_waves(color_pack, 5, 18, 60)
+        elif choice == 6:
+            print('color_cycle_fade')
+            effects.color_cycle_fade(0.005, color_pack, 20)
 
 
 if __name__ == '__main__':
@@ -52,4 +60,5 @@ if __name__ == '__main__':
     # parser.add_argument()
     args = parser.parse_args()
     random_effects()
+    # effects.water_waves(EASTER, 5, 18, -1)
     print("ending program")
